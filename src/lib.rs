abf92c4e6146118e9d3dd5b1a7a0d2ef889569ca
ezomo <ezomo1daniel@gmail.com>
2025-09-25 17:18:08 +0900

    add axes

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod p9n_interface;
 pub mod ps4_dualshock4;
 pub mod ps5_dualsense;
 
-pub use p9n_interface::{Button, Gamepad};
+pub use p9n_interface::{Axes, Button, Gamepad};
 pub use ps4_dualshock4::DualShock4Layout;
 pub use ps5_dualsense::DualSenseLayout;
 use safe_drive::msg::common_interfaces::sensor_msgs;
